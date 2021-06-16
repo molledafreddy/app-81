cc522acdae525ba61785d1a1d408bc6b567d8185
Freddy MOlleda <molledafreddy@gmail.com>
2021-06-16 19:41:44 -0400

    se crearon modelos, factories y migraciones con la estructura de datos

---
+++ database/factories/ShippingAddressFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ShippingAddress;
+use Faker\Generator as Faker;
+
+$factory->define(ShippingAddress::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2021_06_16_043300_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->lenght(100);
+            $table->text('description');
+            $table->integer('existence');
+            $table->double('discount');
+            $table->double('total');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
